6187f6801c0f41e1c59079a7540b35a7290c1971
Taulant Ndreka <tndreka@1-E-9.42heilbronn.de>
2024-07-23 22:36:54 +0200

    did the conversion of the mandelbrod scale from my window

--- src/mandelbrot.c
+++ src/mandelbrot.c
@@ -6,8 +6,40 @@
 /*   By: tndreka <tndreka@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 21:26:16 by tndreka           #+#    #+#             */
-/*   Updated: 2024/07/23 19:53:08 by tndreka          ###   ########.fr       */
+/*   Updated: 2024/07/23 22:24:40 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
+
+
+/* NEEd to chec k if the point given is in the mandelbrot scale 
+*/
+
+
+//this function puts the specific range of the complex values of mandelbro
+// X(-2 .. 0.5) [real] || Y(-1 .. 1)[imaginary] and converts it with our 
+// HEIGHT AND WIDTH ==> transform the condinat  -22 to +2 to the scale 800
+
+void pixel_trick(int x, int y, t_frac *fractal)
+{
+		
+}
+
+void fractol(t_frac *fractal)
+{
+	int x;
+	int y;
+
+	y = -1;
+	while(HEIGHT > y)
+	{
+		x = -1;
+		while(WIDTH > x)
+		{
+			pixel_trick(x, y, fractal);
+			x++;
+		}
+		y++;
+	}
+}
--- src/ultils_calculator.c
+++ src/ultils_calculator.c
@@ -6,38 +6,40 @@
 /*   By: tndreka <tndreka@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 01:10:10 by tndreka           #+#    #+#             */
-/*   Updated: 2024/07/23 20:02:13 by tndreka          ###   ########.fr       */
+/*   Updated: 2024/07/23 22:33:41 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "fractol.h"
+// #include "fractol.h"
 /* NEEd to chec k if the point given is in the mandelbrot scale 
 */
 
-
+#include <stdio.h>
 //this function puts the specific range of the complex values of mandelbro
 // X(-2 .. 0.5) [real] || Y(-1 .. 1)[imaginary] and converts it with our 
 // HEIGHT AND WIDTH ==> transform the condinat  -22 to +2 to the scale 800
 
-void pixel_trick(int x, int y, t_frac *fractal)
+//on the function Z = Z^2 + c;  ===> in the first iteration we know that the first poiint 
+//its Z(0, 0) so out old min is Z(0,0) ==> so we can remove as the parameter the old_min 
+// because we already know the value ... 
+// double scale_calc(double unsaled_n, double new_min, double new_max, double old_min, double old_max)
+// {
+// 	return (new_max - new_min) * (unsaled_n - old_min)/(old_max - old_min) + new_min;
+// }int main ()
+// {
+// 	for(int i=0; i < 800; ++i)
+// 	{
+// 	printf("%d -> %f\n",i ,scale_calc((double)i, -2, +2, 0, 799));	
+// 	}
+// }
+double scale_calc(double unsaled_n, double new_min, double new_max, double old_max)
 {
-		
+	return (new_max - new_min) * (unsaled_n /old_max) + new_min;
 }
-
-void fractol(t_frac *fractal)
+int main ()
 {
-	int x;
-	int y;
-
-	y = -1;
-	while(HEIGHT > y)
+	for(int i=0; i < 800; ++i)
 	{
-		x = -1;
-		while(WIDTH > x)
-		{
-			pixel_trick(x, y, fractal)''
-			x++;
-		}
-		y++;
+	printf("%d -> %f\n",i ,scale_calc((double)i, -2, +2, 799));	
 	}
 }
